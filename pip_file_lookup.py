87794bdbc5185cbe2c67e837d663332f2c980e98
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2018-03-27 13:43:05 -0500

    Simplify and avoid importing private packages.

--- pip_file_lookup.py
+++ pip_file_lookup.py
@@ -1,26 +1,22 @@
 #! /usr/bin/env python3
 
 import os
-import pip._vendor
-import pip._vendor.packaging.utils
 import sys
+import pip.utils
 
 if __name__ == '__main__':
-
-    for dist in pip._vendor.pkg_resources.working_set:
-        name = pip._vendor.packaging.utils.canonicalize_name(dist.project_name)
-
-        if isinstance(dist, pip._vendor.pkg_resources.DistInfoDistribution):
-            # RECORDs should be part of .dist-info metadatas
-            if dist.has_metadata('RECORD'):
-                lines = dist.get_metadata_lines('RECORD')
-                paths = [l.split(',')[0] for l in lines]
-                paths_absolute = [os.path.join(dist.location, p) for p in paths]
+    for dist in pip.utils.get_installed_distributions():
+        # RECORDs should be part of .dist-info metadatas
+        if dist.has_metadata('RECORD'):
+            lines = dist.get_metadata_lines('RECORD')
+            paths = [l.split(',')[0] for l in lines]
+            paths_absolute = [os.path.join(dist.location, p) for p in paths]
+        # Otherwise use pip's log for .egg-info's
+        elif dist.has_metadata('installed-files.txt'):
+            paths = dist.get_metadata_lines('installed-files.txt')
+            paths_absolute = [os.path.join(dist.egg_info, p) for p in paths]
         else:
-            # Otherwise use pip's log for .egg-info's
-            if dist.has_metadata('installed-files.txt'):
-                paths = dist.get_metadata_lines('installed-files.txt')
-                paths_absolute = [os.path.join(dist.egg_info, p) for p in paths]
+            sys.stderr.write('Cannot get files for pkg: {}'.format(dist.project_name))
 
         if sys.argv[1] in paths_absolute:
-            print(name)
+            print(dist.project_name)
